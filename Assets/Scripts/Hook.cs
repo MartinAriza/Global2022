76c8c32d02117d386c28d278717b8ea6cd876a0a
Kai <danielperelu@gmail.com>
2022-01-29 15:05:12 +0100

    Lista que crea cuerda al mantener espacio y la destruye al soltar

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Hook : MonoBehaviour
 {
 
@@ -10,55 +11,79 @@ public class Hook : MonoBehaviour
     public GameObject ropePart;
 
     Rigidbody rb;
+    Vector3 fwd;
+    Vector3 ropePos;
+    GameObject first;
+    GameObject second;
+
+    Queue<GameObject> ropePool;
 
     private void Start()
     {
+        ropePool = new Queue<GameObject>();
+
         rb = GetComponent<Rigidbody>();
+
+        
     }
+
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        Vector3 ropePos = new Vector3(transform.position.x + forwardPos, transform.position.y, transform.position.z);
 
         //prueba movimiento
         //int velmove = 10;
-        //float movx = Input.GetAxis("horizontal");
+        //float movx = Input.GetAxis("Horizontal");
         //rb.velocity = new Vector3(movx * velmove, rb.velocity.y, 0);
 
         Debug.DrawRay(transform.position, transform.position + fwd * ropeLength, Color.green);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject first = Instantiate(ropePart, ropePos, Quaternion.identity);
+            fwd = transform.TransformDirection(Vector3.forward);
+            ropePos = new Vector3(transform.position.x + forwardPos, transform.position.y, transform.position.z);
+
+            first = Instantiate(ropePart, ropePos, Quaternion.identity);
             GetComponent<FixedJoint>().connectedBody = first.GetComponent<Rigidbody>();
+            ropePool.Enqueue(first);
 
             if (Physics.Raycast(transform.position, fwd, out hit, ropeLength))
             {
-                for(int i = 1; i < hit.distance; i++)
+                for(int i = 0; i < hit.distance; i++)
                 {
                     Vector3 newPos = new Vector3(ropePos.x + i, ropePos.y, ropePos.z);
-                    GameObject second = Instantiate(ropePart, newPos, Quaternion.identity);
+                    second = Instantiate(ropePart, newPos, Quaternion.identity);
                     first.GetComponent<FixedJoint>().connectedBody = second.GetComponent<Rigidbody>();
                     first = second;
+                    ropePool.Enqueue(first);
                 }
 
                 first.GetComponent<FixedJoint>().connectedBody = hit.collider.gameObject.GetComponent<Rigidbody>();
 
             }
-
             else
             {
-                for (int i = 1; i < ropeLength; i++)
+                for (int i = 0; i < ropeLength; i++)
                 {
                     Vector3 newPos = new Vector3(ropePos.x + i, ropePos.y, ropePos.z);
-                    GameObject second = Instantiate(ropePart, newPos, Quaternion.identity);
+                    second = Instantiate(ropePart, newPos, Quaternion.identity);
                     first.GetComponent<FixedJoint>().connectedBody = second.GetComponent<Rigidbody>();
                     first = second;
+                    ropePool.Enqueue(first);
                 }
             }
 
         }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            while(ropePool.Count > 0)
+            {
+                GameObject go = ropePool.Dequeue();
+                Destroy(go);
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/RopePool.cs
@@ -0,0 +1,68 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class RopePool : MonoBehaviour
+//{
+//    [System.Serializable]
+//    public class Pool
+//    {
+//        public string tag;
+//        public GameObject prefab;
+//        public int size;
+//    }
+
+//    #region Singleton
+
+//    public static RopePool Instance;
+
+//    private void Awake()
+//    {
+//        Instance = this;
+//    }
+
+//    #endregion
+
+//    public List<Pool> pools;
+//    public Dictionary<string, Queue<GameObject>> poolDict;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        poolDict = new Dictionary<string, Queue<GameObject>>();
+
+//        foreach(Pool pool in pools)
+//        {
+//            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+//            for (int i = 0; i < pool.size; i++)
+//            {
+//                GameObject obj = Instantiate(pool.prefab);
+//                obj.SetActive(false);
+//                objectPool.Enqueue(obj);
+//            }
+
+//            poolDict.Add(pool.tag, objectPool);
+//        }
+//    }
+
+//    public GameObject SpawnFromPool (string tag, Vector3 position, Quaternion rotation)
+//    {
+//        if (!poolDict.ContainsKey(tag))
+//        {
+//            Debug.LogWarning("Pool with tag " + tag + "doesn't exist.");
+//            return null;
+//        }
+
+//        GameObject objectToSpawn = poolDict[tag].Dequeue();
+
+//        objectToSpawn.SetActive(true);
+//        objectToSpawn.transform.position = position;
+//        objectToSpawn.transform.rotation = rotation;
+
+//        poolDict[tag].Enqueue(objectToSpawn);
+
+//        return objectToSpawn;
+//    }
+
+//}
